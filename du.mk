5aae2733c02be75a4c9f769fc6ac800cd2c7e044
Bret Zamzow <mazwoz@gmail.com>
2013-10-15 18:23:15 -0700

    Updated cm -> du

--- du.mk
+++ du.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/du/configs/cdma.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := l900
@@ -9,14 +9,14 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/configs/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/l900/full_l900.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := l900
-PRODUCT_NAME := cm_l900
+PRODUCT_NAME := du_l900
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SPH-L900
 PRODUCT_MANUFACTURER := samsung
